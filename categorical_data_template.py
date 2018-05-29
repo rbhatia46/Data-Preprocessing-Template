b69270e4b7f4b16eb2b59d0b21c0af22b5ec82c3
Rahul Bhatia <rbhatia46@gmail.com>
2018-05-29 12:41:50 +0530

    Initial Commit

---
+++ categorical_data_template.py
@@ -0,0 +1,37 @@
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values # Independent Variables
+y = dataset.iloc[:, 3].values   # Dependent Variables
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+# Encoding the Dependent Variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+
+# Splitting the dataset into Training Set and Test set
+from sklearn.cross_validation import train_test_split
+X_train,X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
+
+
---
+++ missing_data_template.py
@@ -0,0 +1,27 @@
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values # Independent Variables
+y = dataset.iloc[:, 3].values   # Dependent Variables
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Splitting the dataset into Training Set and Test set
+from sklearn.cross_validation import train_test_split
+X_train,X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
